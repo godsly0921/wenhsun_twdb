aa49652e8ac1acde9b97653928b2b353ca3da6a0
Eric G. Huang <eric.g.yuan@gmail.com>
2019-04-04 01:29:43 +0800

    把登入頁先套好新版面

--- protected/views/admin/login.php
+++ protected/views/admin/login.php
@@ -1,3 +1,4 @@
+<!--
 <script>
 	$(function() {
 		if ($('#success_msg').html() != '') {
@@ -40,6 +41,7 @@
                                 <div class="form-group">
                                     <input class="form-control" placeholder="Password" name="password" type="password" value="">
                                 </div>
+
                                 <div class="form-group">
                                     <select class="form-control" name="login_type">
                                                 <option selected="selected" value="1">使用者</option>
@@ -67,3 +69,66 @@
             </div>
         </div>
     </div>
+-->
+
+<body class="login">
+<style>
+    .login_content h1:after, .login_content h1:before {
+        content: "";
+        height: 1px;
+        position: absolute;
+        top: 10px;
+        width: 10%;
+    }
+</style>
+<div>
+    <?php if (isset(Yii::app()->session['message']) && Yii::app()->session['message'] !== ''): ?>
+        <div id="error_alert" class="alert alert-danger alert-dismissible fade in" role="alert">
+            <?php echo Yii::app()->session['message'];?>
+            <?php unset(Yii::app()->session['message']);?>
+        </div>
+    <?php endif; ?>
+    <div class="login_wrapper">
+
+        <div class="animate form login_form">
+            <section class="login_content">
+                <form role="form" action="<?php echo Yii::app()->createUrl('admin/auth'); ?>" method="post" accept-charset="utf-8">
+                    <h1>文訊雜誌社人資管理系統</h1>
+
+                    <div>
+                        <input type="text" class="form-control" name="user_account" placeholder="Username" required="required" autofocus/>
+                    </div>
+                    <div>
+                        <input type="password" class="form-control" name="password" placeholder="Password" required="required" />
+                    </div>
+                    <div>
+                        <select class="form-control" name="login_type">
+                            <option value="1" selected="selected">使用者</option>
+                            <option value="0">系統管理員</option>
+                        </select>
+                    </div>
+                    <div class="clearfix"></div>
+                    <div class="separator">
+
+                        <p class="change_link">
+                            <a href="<?php echo Yii::app()->createUrl('/admin/register');?>" class="to_register">註冊會員</a>
+                            <input style="float:none;" type="submit" class="btn btn-default submit" value="登入"/>
+                        </p>
+
+                        <div class="clearfix"></div>
+                        <br />
+                    </div>
+                </form>
+            </section>
+        </div>
+    </div>
+</div>
+<script>
+    $(function() {
+        if ($('#error_alert').html() != '') {
+            $('#error_alert').show().fadeOut(3500);
+        }
+    });
+</script>
+</body>
+
--- protected/views/layouts/admin.php
+++ protected/views/layouts/admin.php
@@ -1,17 +1,16 @@
 <!DOCTYPE html>
 <html lang="zh-tw">
 <head>
-	<meta charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/assets/admin/ext/css/bootstrap.min.css" />
-    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/assets/admin/ext/css/metisMenu.min.css" />
-    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/assets/admin/ext/css/font-awesome.min.css" />
-    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/assets/admin/ext/css/dataTables.bootstrap.css" />
-    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/assets/admin/ext/css/sb-admin-2.css" />
-    <script src="<?php echo Yii::app()->request->baseUrl; ?>/assets/admin/ext/js/jquery.min.js"></script>
-    <script src="<?php echo Yii::app()->request->baseUrl; ?>/assets/admin/ext/js/bootstrap.min.js"></script>
-    <script src="<?php echo Yii::app()->request->baseUrl; ?>/assets/admin/ext/js/metisMenu.min.js"></script>
-    <script src="<?php echo Yii::app()->request->baseUrl; ?>/assets/admin/ext/js/sb-admin-2.js"></script>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>文訊雜誌社人資系統</title>
+    <link href="<?php echo Yii::app()->request->baseUrl; ?>/assets/gentelella/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo Yii::app()->request->baseUrl; ?>/assets/gentelella/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="<?php echo Yii::app()->request->baseUrl; ?>/assets/gentelella/build/css/custom.min.css" rel="stylesheet">
+    <script src="<?php echo Yii::app()->request->baseUrl; ?>/assets/gentelella/vendors/jquery/dist/jquery.min.js"></script>
+    <script src="<?php echo Yii::app()->request->baseUrl; ?>/assets/gentelella/vendors/validator/validator.js"></script>
 </head>
 <?= $content ?>
+</html>
