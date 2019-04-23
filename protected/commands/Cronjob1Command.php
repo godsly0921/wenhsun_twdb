6126f8b71d01dabf37521fb1039f71c369d43320
郭信良 <godsly0921@gmail.com>
2019-04-23 23:22:46 +0800

    處理正式機讀不到卡機資料的問題

--- protected/commands/Cronjob1Command.php
+++ protected/commands/Cronjob1Command.php
@@ -1,7 +1,7 @@
 <?php
 class Cronjob1Command extends CConsoleCommand{
 	
-	public function run(){
+	public function run($today = null){
       
       $res = new CronService();
       $f   = $res->today_record();
