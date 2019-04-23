015eee9e014a629a8d4ecb7d57521472c5c788bd
Eric G. Huang <eric.g.yuan@gmail.com>
2019-04-23 20:05:38 +0800

    #3 修改員工管理 migration file 順序 才不會造成在正式環境建表時會出錯的麻煩

--- migration/employee.sql
+++ migration/employee.sql
@@ -1,4 +1,25 @@
-CREATE TABLE `employee_info` (
+CREATE TABLE `employee_seats` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `seat_name` varchar(64) NOT NULL DEFAULT '',
+  `seat_number` varchar(8) NOT NULL DEFAULT '',
+  `create_at` datetime NOT NULL,
+  `update_at` datetime NOT NULL,
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `employee_seats` ADD UNIQUE (seat_number, seat_name);
+
+CREATE TABLE `employee_extensions` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `ext_number` INT(8) NOT NULL,
+  `create_at` datetime NOT NULL,
+  `update_at` datetime NOT NULL,
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `employee_extensions` ADD UNIQUE (ext_number);
+
+CREATE TABLE `employee` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `user_name` varchar(64) NOT NULL,
   `password` varchar(128) NOT NULL,
@@ -28,10 +49,10 @@ CREATE TABLE `employee_info` (
 PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE `employee_info` ADD UNIQUE (user_name);
+ALTER TABLE `employee` ADD UNIQUE (user_name);
 
-ALTER TABLE employee_info
+ALTER TABLE employee
 ADD FOREIGN KEY (ext_num) REFERENCES employee_extensions (id);
 
-ALTER TABLE employee_info
+ALTER TABLE employee
 ADD FOREIGN KEY (seat_num) REFERENCES employee_seats (id);
