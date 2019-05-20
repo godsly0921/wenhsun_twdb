319ddbb28190efed880ca75127f8b757efa68470
Sunny CF Hsiao <sunnycfhsiao@kkbox.com>
2019-05-20 15:49:43 +0800

    Fix email cannot be null when update member data

--- protected/controllers/MemberController.php
+++ protected/controllers/MemberController.php
@@ -788,7 +788,7 @@ class MemberController extends Controller
         $inputs['id'] = filter_input(INPUT_POST, 'id');
         $inputs['account'] = filter_input(INPUT_POST, 'account');
         $inputs['name'] = filter_input(INPUT_POST, 'name');
-        $inputs['email'] = filter_input(INPUT_POST, 'email');
+        $inputs['email'] = filter_input(INPUT_POST, 'account');
         $inputs['gender'] = filter_input(INPUT_POST, 'gender');
         $inputs['birthday'] = filter_input(INPUT_POST, 'year') . '-' .
             filter_input(INPUT_POST, 'month') . '-' . filter_input(INPUT_POST, 'day');
