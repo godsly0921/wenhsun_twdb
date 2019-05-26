1ded71b3836cd39653b8f37364905c66e24670a3
郭信良 <godsly0921@gmail.com>
2019-05-27 01:12:08 +0800

    #修正正式機連結錯誤的問題

--- protected/views/salary/report/employee.php
+++ protected/views/salary/report/employee.php
@@ -143,7 +143,7 @@
                             <div class="form-group">
                                 <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
                                     <button type="submit" class="btn btn-primary">修改</button>
-                                    <a class="btn btn-default pull-right" href="/salary/report/batch?batchId=<?=$data->getBatchId()?>">返回</a>
+                                    <a class="btn btn-default pull-right" href="<?= Yii::app()->createUrl('/salary/report/batch?batchId=');?><?=$data->getBatchId()?>">返回</a>
                                 </div>
                             </div>
                         </form>
