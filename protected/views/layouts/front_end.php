9a0695d87821a709142b685019b9ae8bec882bfa
oopspan <yuyen103@gmail.com>
2019-06-10 23:26:41 +0800

    切圖程式 dpi 調整

---
+++ protected/views/layouts/front_end.php
@@ -0,0 +1,52 @@
+<html>
+  <head>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <meta charset="utf-8">
+    <meta name="keywords" content='' />
+    <meta name="description" content=''/>
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans+TC&amp;subset=chinese-traditional,japanese" rel="stylesheet" class="next-head">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <title>台灣文學照片資料庫</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="../css/layout.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    <!-- Header --- Start -->
+    <nav class="navbar navbar-light bg-lignt navbar-expand-md header_navbar">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-nav">
+          <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="navbar-collapse collapse dual-nav order-1 order-md-1 justify-content-end">
+          <ul class="navbar-nav header_navbar_nav">
+              <li class="nav-item">
+                  <a class="nav-link" href="#">關於我們</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="#">最新消息</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="#">圖片專欄</a>
+              </li>
+          </ul>
+      </div>
+      <a href="/" class="navbar-brand mx-auto order-0 order-md-2 p-2">台灣文學照片資料庫</a>
+      <div class="navbar-collapse collapse dual-nav order-2 order-md-2 justify-content-end">
+          <ul class="navbar-nav header_navbar_nav">
+              <li class="nav-item">
+                  <a class="nav-link" href="#">圖片檢索</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="#">我的下載</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="#">會員註冊/登入</a>
+              </li>
+          </ul>
+      </div>
+    </nav>  
+    <!--   Header  End -->
+  </body>
+</html>
