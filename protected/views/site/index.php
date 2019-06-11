f4940ef6360beafdccff23061395f212aba03e0b
oopspan <yuyen103@gmail.com>
2019-06-12 00:45:55 +0800

    版面調整

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -11,28 +11,30 @@
   <img src="<?= Yii::app()->request->baseUrl; ?>/assets/image/banner/banner_4.jpg">
   <img src="<?= Yii::app()->request->baseUrl; ?>/assets/image/banner/banner_5.jpg"> -->
 </div>
-<form name="group_form" class="form-horizontal" action="#" method="get">
-  <div class="col-lg-8 mx-auto input-group input-group-lg my-5">
-    <input type="text" class="form-control" placeholder="推薦關鍵字：洛夫" aria-label="推薦關鍵字：洛夫" aria-describedby="basic-addon2">
-    <div class="input-group-append">
-      <button class="btn btn-outline-light customer_search_button" type="submit">搜尋</button>
+<div class="container">
+  <form name="group_form" class="form-horizontal" action="#" method="get">
+    <div class="col-lg-9 mx-auto input-group input-group-lg my-5">
+      <input type="text" class="form-control" placeholder="推薦關鍵字：洛夫" aria-label="推薦關鍵字：洛夫" aria-describedby="basic-addon2">
+      <div class="input-group-append">
+        <button class="btn btn-outline-light customer_search_button" type="submit">搜尋</button>
+      </div>
     </div>
+  </form>
+  <div class="py-5" id="ad_image">
+      <div><img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/image/AD/1.jpg"></div>
+      <div><img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/image/AD/2.jpg"></div>
+      <div><img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/image/AD/3.jpg"></div>
+      <div><img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/image/AD/4.jpg"></div>
+      <div><img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/image/AD/5.jpg"></div>
+      <div><img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/image/AD/6.jpg"></div>
+      <div><img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/image/AD/7.jpg"></div>
+      <div><img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/image/AD/8.jpg"></div>
+      <div><img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/image/AD/9.jpg"></div>
+      <div><img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/image/AD/10.jpg"></div>
+      <div><img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/image/AD/11.jpg"></div>
+      <div><img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/image/AD/12.jpg"></div>
+      <div><img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/image/AD/13.jpg"></div>
   </div>
-</form>
-<div class="container py-5" id="ad_image">
-    <div><img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/image/AD/1.jpg"></div>
-    <div><img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/image/AD/2.jpg"></div>
-    <div><img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/image/AD/3.jpg"></div>
-    <div><img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/image/AD/4.jpg"></div>
-    <div><img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/image/AD/5.jpg"></div>
-    <div><img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/image/AD/6.jpg"></div>
-    <div><img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/image/AD/7.jpg"></div>
-    <div><img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/image/AD/8.jpg"></div>
-    <div><img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/image/AD/9.jpg"></div>
-    <div><img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/image/AD/10.jpg"></div>
-    <div><img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/image/AD/11.jpg"></div>
-    <div><img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/image/AD/12.jpg"></div>
-    <div><img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/image/AD/13.jpg"></div>
 </div>
 <script type="text/javascript">
   $(document).ready( function() {
@@ -44,7 +46,7 @@
     });
 
     $('#banner').slick({
-        dots: false,
+        dots: true,
         infinite: true,
         arrows: false,
         autoplay: true,
