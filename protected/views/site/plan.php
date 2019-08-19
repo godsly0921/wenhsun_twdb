4fbdac5c955e4d79f9a22242754e64559d15d98f
Sunny CF Hsiao <sunnycfhsiao@kkbox.com>
2019-08-20 00:27:23 +0800

    新增方案選擇頁面

---
+++ protected/views/site/plan.php
@@ -0,0 +1,67 @@
+<div id="banner" class="row"></div>
+<div class="container">
+    <div id="title" class="text-center">挑選適合您的方案</div>
+    <div class="type">
+        <div class="row">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-3 text-center">
+                <img src="<?= Yii::app()->request->baseUrl . '/assets/image/plan1.png' ?>" class="img-circle">
+                <br>
+                <p class="user">老師、學生、研究者</p>
+                <p class="description">
+                    可以使用本圖庫尺寸為 S﹙72dpi﹚<br>
+                    或 M﹙96dpi﹚的尺寸。可用於書面<br>
+                    報告、簡報檔或一般書面印刷使用。<br>
+                    亦可用於網路文章配圖。
+                </p>
+            </div>
+            <div class="col-sm-1"></div>
+            <div class="col-sm-3 text-center">
+                <img src="<?= Yii::app()->request->baseUrl . '/assets/image/plan2.png' ?>" class="img-circle">
+                <br>
+                <p class="user">出版社、媒體廣告</p>
+                <p class="description">
+                    可以使用本圖庫尺寸為 L﹙150dpi﹚<br>
+                    或 XL﹙300dpi﹚的尺寸。可作圖書<br>
+                    封面、內頁的印刷用，媒體廣告可依<br>
+                    其使用性質，選用 XL 或更大的尺寸<br>
+                    ﹙請直接接洽文訊﹚。<br>
+                    *所有相關出版品、海報或實體的產<br>
+                    出品，請提供三份供文訊留存。
+                </p>
+            </div>
+            <div class="col-sm-1"></div>
+            <div class="col-sm-3 text-center">
+                <img src="<?= Yii::app()->request->baseUrl . '/assets/image/plan1.png' ?>" class="img-circle">
+                <br>
+                <p class="user">企業、活動</p>
+                <p class="description">
+                    請直接接洽文訊，我們將依使用範<br>
+                    圍、性質、曝光度另外報價。
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+<style type="text/css">
+    #title {
+        padding-bottom: 80px;
+        font-size: 48px;
+    }
+
+    .user {
+        font-size: 20px;
+        color: #dc5514;
+        padding-top: 25px;
+        padding-bottom: 25px;
+    }
+
+    .description {
+        font-size: 12px;
+        padding-bottom: 45px;
+    }
+
+    #banner {
+        padding-bottom: 85px;
+    }
+</style>
