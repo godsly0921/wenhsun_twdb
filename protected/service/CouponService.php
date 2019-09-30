c21055b852a00b427857807248f2c50c2c1e77e7
Sunny CF Hsiao <sunnycfhsiao@kkbox.com>
2019-09-30 11:18:23 +0800

    修正優惠券編輯錯誤

--- protected/service/CouponService.php
+++ protected/service/CouponService.php
@@ -100,12 +100,12 @@ class CouponService
         if (!$model->hasErrors()) {
             if( $model->save() ){
                 $motion = "更新優惠";
-                $log = "更新 優惠編號 = " . $id . "；優惠名稱 = " . $inputs["coupon_name"];
+                $log = "更新 優惠編號 = " . $input['id'] . "；優惠名稱 = " . $input["coupon_name"];
                 $operationlogService->create_operationlog( $motion, $log );
                 return array(true,'修改成功',$model);         
             }else{       
                 $motion = "更新優惠";
-                $log = "更新 優惠編號 = " . $id . "；優惠名稱 = " . $inputs["coupon_name"];
+                $log = "更新 優惠編號 = " . $input['id']. "；優惠名稱 = " . $input["coupon_name"];
                 $operationlogService->create_operationlog( $motion, $log, 0 );
                 return array(false,$model->getErrors());
             }
