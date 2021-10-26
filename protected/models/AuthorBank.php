e5a83b032549d74cdfaf969ddefba7fc2baabc29
oopspan <yuyen103@gmail.com>
2021-10-26 23:15:29 +0800

    銀行代碼、分行代碼、帳號開放最多50個字

--- protected/models/AuthorBank.php
+++ protected/models/AuthorBank.php
@@ -24,10 +24,10 @@ class AuthorBank extends CActiveRecord
     {
         return [
             ['bank_name', 'length', 'min' => 1, 'max' => 50, 'tooLong' => "銀行名稱最多50個字元"],
-            ['bank_code', 'length', 'min' => 1, 'max' => 3, 'tooLong' => "銀行代碼最多3個字元"],
+            ['bank_code', 'length', 'min' => 1, 'max' => 50, 'tooLong' => "銀行代碼最多50個字元"],
             ['branch_name', 'length', 'min' => 1, 'max' => 50, 'tooLong' => "分行名稱最多50個字元"],
-            ['branch_code', 'length', 'min' => 1, 'max' => 8, 'tooLong' => "分行代碼最多8個字元"],
-            ['bank_account', 'length', 'min' => 1, 'max' => 20, 'tooLong' => "帳號最多20個字元"],
+            ['branch_code', 'length', 'min' => 1, 'max' => 50, 'tooLong' => "分行代碼最多50個字元"],
+            ['bank_account', 'length', 'min' => 1, 'max' => 50, 'tooLong' => "帳號最多50個字元"],
             ['account_name', 'length', 'min' => 1, 'max' => 50, 'tooLong' => "戶名最多50個字元"],
         ];
 
