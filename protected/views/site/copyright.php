8a64ea3f291e87fafa69e0082b32083fc38f475a
oopspan <yuyen0619@gmail.com>
2025-10-08 21:33:25 +0800

    著作權聲明

--- protected/views/site/copyright.php
+++ protected/views/site/copyright.php
@@ -1,16 +1,3 @@
-<div id="banner"></div>
-<div class="container">
-    <div class="paragraph">
-        <p class="header">台灣文學照片資料庫　著作權聲明</p>
-        <p style="font-family:times new roman,times,serif;font-size: 14px">《台灣文學照片資料庫》網站所刊載之所有內容，包括但不限於文字、照片、影像、插圖、錄音、影音等素材，均受《中華民國著作權法》及相關國際著作權法令之保護，其著作權為本網站管理單位及相關內容提供者所有。</p>
-
-        <p style="font-family:times new roman,times,serif;font-size: 14px">本網站提供之內容僅供一般大眾瀏覽使用，未經授權，不得下載、重製、改作、散布、發行、公開展示或以任何形式利用全部或部分內容。</p>
-
-        <p style="font-family:times new roman,times,serif;font-size: 14px">如有轉載、引用或其他使用需求，請事先以書面方式向本網站申請授權，並於經同意使用時，明確標示出處為「台灣文學照片資料庫」。</p>
-
-        <p style="font-family:times new roman,times,serif;font-size: 14px">授權申請請洽： <a href="mailto:wsnlib@gmail.com">wsnlib@gmail.com</a></p>
-    </div>
-</div>
 <style type="text/css">
     .header {
         font-size: 23px;
@@ -30,7 +17,63 @@
         padding-bottom: 0px;
     }
 
-    #banner {
-        padding-bottom: 85px;
+    /* #banner {
+        padding-bottom: 34px;
+    } */
+
+    .title_box{
+        display: flex;
+        flex-wrap: nowrap;
+        align-items: center;
+        gap: 10px;
+        padding: 34px 0px 30px 0px;
+    }
+    .title_border{
+        width: 4px;
+        height: 24px;
+        background-color: #AC7361;
+        border-radius: 30px;
+    }
+    .title_text{
+        color: #1E1E1E;
+        font-weight: bold;
+        font-size: 20px;
+    }
+    .content_box{
+        width: 100%;
+        height: 640px;
+        background-color: rgba(255, 255, 255, 0.3);
+        padding: 34px;
+        border-radius: 30px;
+        font-size: 16px;
+        line-height: 24px;
+        margin-bottom: 30px;
+    }
+    .highlight_auth{
+        font-size: 16px;
+        font-weight: bold;
+        color: #AC7361;
+        padding-top: 20px;
+    }
+    .highlight_auth a{
+        color: #AC7361;
+        text-decoration: underline;
     }
 </style>
+<div id="banner"></div>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-8 mx-auto">
+            <div class="title_box">
+                <div class="title_border"></div>
+                <div class="title_text">著作權聲明</div>
+            </div>
+            <div class="content_box">
+                <p>《台灣文學照片資料庫》網站所刊載之所有內容，包括但不限於文字、照片、影像、插圖、錄音、影音等素材，均受《中華民國著作權法》及相關國際著作權法令之保護，其著作權為本網站管理單位及相關內容提供者所有。</p>
+                <p>本網站提供之內容僅供一般大眾瀏覽使用，未經授權，不得下載、重製、改作、散布、發行、公開展示或以任何形式利用全部或部分內容。</p>
+                <p>如有轉載、引用或其他使用需求，請事先以書面方式向本網站申請授權，並於經同意使用時，明確標示出處為「台灣文學照片資料庫」。</p>
+                <div class="highlight_auth">授權申請請洽：<a href="mailto:wsnlib@gmail.com">wsnlib@gmail.com</a></div>
+            </div>
+        </div>
+    </div>
+</div>
